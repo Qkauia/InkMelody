d61f7d8ac3f6f819ecca5ebdb06adb748e99b969
Qkauia <kauia96@gmail.com>
2023-11-16 22:55:16 +0800

    新增隱私權頁面以及更改首頁畫面

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  get "about", to: "pages#about"
   # get "about_us", to: "pages#about"
+  get "about", to: "pages#about"
+  get "privacy", to: "pages#privacy"
+  root "welcome#index"
 
   
 end
