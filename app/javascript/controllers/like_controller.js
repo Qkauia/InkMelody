c01bf64a6ab3db1b439b6067da00a982ede7473e
Qkauia <kauia96@gmail.com>
2023-12-05 14:21:16 +0800

    request.js add & likebtn finish

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import { Controller } from "@hotwired/stimulus";
+import { patch } from "@rails/request.js";
 
 const LIKE_LABEL = "收藏";
 const UNLIKE_LABEL = "取消";
@@ -17,33 +18,43 @@ export default class extends Controller {
     }
   }
 
-  toggle(e) {
+  async toggle(e) {
     e.preventDefault();
 
     const { id } = this.element.dataset;
     const url = `/api/v1/products/${id}/like`;
-    const token = document.querySelector("meta[name='csrf-token']").content;
 
-    fetch(url, {
-      method: "PATCH",
-      headers: {
-        "X-CSRF-Token": token,
-      },
-    })
-      .then((result) => {
-        return result.json();
-      })
-      .then(({ id, status }) => {
-        console.log(id, status);
+    const response = await patch(url);
+    if (response.ok) {
+      const { status } = await response.json;
+      if (status == "liked") {
+        this.btnTarget.textContent = UNLIKE_LABEL;
+      } else {
+        this.btnTarget.textContent = LIKE_LABEL;
+      }
+    }
+    // const token = document.querySelector("meta[name='csrf-token']").content;
 
-        if (status == "liked") {
-          this.btnTarget.textContent = UNLIKE_LABEL;
-        } else {
-          this.btnTarget.textContent = LIKE_LABEL;
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // fetch(url, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "X-CSRF-Token": token,
+    //   },
+    // })
+    //   .then((result) => {
+    //     return result.json();
+    //   })
+    //   .then(({ id, status }) => {
+    //     console.log(id, status);
+
+    //     if (status == "liked") {
+    //       this.btnTarget.textContent = UNLIKE_LABEL;
+    //     } else {
+    //       this.btnTarget.textContent = LIKE_LABEL;
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   }
 }
