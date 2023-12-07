639210dd27bbef922e3e83f31c634081be8b8728
Qkauia <kauia96@gmail.com>
2023-12-07 21:20:00 +0800

    add flash stimulus controller

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="flash"
+export default class extends Controller {
+  close() {
+    this.element.remove();
+  }
+}
